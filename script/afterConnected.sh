e74943a900f4f2c1b063e440d236a0bef1bafeab
Tommy Tang <twhtanghk@gmail.com>
2016-03-10 11:05:33 +0800

    update script name for user accounts

--- script/afterConnected.sh
+++ script/afterConnected.sh
@@ -6,7 +6,7 @@ root=~/git/server.dns
 HOSTNAME=`hostname`
 
 cd ${root}
-. script/oauth2.sh
+. script/user.sh
 node_modules/.bin/coffee script/record.coffee -u ${oauth2user} -p ${oauth2pass} --del vpn.net ${HOSTNAME} A 
 node_modules/.bin/coffee script/record.coffee -u ${oauth2user} -p ${oauth2pass} --add vpn.net ${HOSTNAME} A ${INTERNAL_IP4_ADDRESS}
 
