8937af5c36418577b90ce5ea5b8e11dcc4c68fe3
Tommy Tang <twhtanghk@gmail.com>
2016-03-10 11:41:09 +0800

    update script to add/del dns record if vpn is connected/disconnected

--- script/afterConnected.sh
+++ script/afterConnected.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
 export CISCO_SPLIT_INC=0
+unset INTERNAL_IP4_DNS
 
 root=~/git/server.dns
 HOSTNAME=`hostname`
 
 cd ${root}
 . script/user.sh
-node_modules/.bin/coffee script/record.coffee -u ${oauth2user} -p ${oauth2pass} --del vpn.net ${HOSTNAME} A 
-node_modules/.bin/coffee script/record.coffee -u ${oauth2user} -p ${oauth2pass} --add vpn.net ${HOSTNAME} A ${INTERNAL_IP4_ADDRESS}
+
+case "${reason}" in
+
+  connect)
+    node_modules/.bin/coffee script/record.coffee -u ${oauth2user} -p ${oauth2pass} --add vpn.net ${HOSTNAME} A ${INTERNAL_IP4_ADDRESS}
+    ;;
+
+  disconnect)
+    node_modules/.bin/coffee script/record.coffee -u ${oauth2user} -p ${oauth2pass} --del vpn.net ${HOSTNAME} A 
+    ;;
+
+esac
 
 . /usr/share/vpnc-scripts/vpnc-script
